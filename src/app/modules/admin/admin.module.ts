fee719f17b3df7b21040010015554d4b1c7d3ac3
vinisvp <viniciuspedro0905@gmail.com>
2024-08-27 16:51:42 -0300

    Created the categories component

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoriesComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CategoriesComponent
   ]
 })
 export class AdminModule { }
---
+++ src/app/modules/admin/pages/categories/categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent {
+
+}
