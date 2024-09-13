e96a28bf4edcc73784eadd2691d7962021a69e13
João Otávio <jofs2006@gmail.com>
2024-09-13 14:37:02 -0300

    :sparkles: feat: Added more assets on items's categories

--- src/app/modules/checkout/pages/home/home.component.ts
+++ src/app/modules/checkout/pages/home/home.component.ts
@@ -17,5 +17,18 @@ export class HomeComponent {
     itens: []
   };
 
-  
+  selectedCategory: string = '';
+  selectedItems: string[] = [];
+
+  onCategoriaChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedCategory = selectElement.value;
+  }
+
+  addItem(item: string) {
+    if (!this.selectedItems.includes(item)) {
+      this.selectedItems.push(item);
+    }
+  }
+
 }
