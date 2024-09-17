4161c5c97b7b8741ad02ce0fa12b3d4a668ccae5
muriisantuss <muriisantuss@gmail.com>
2024-09-17 15:19:51 -0300

    ✨ feat: creation of the 'main' page

--- src/app/modules/costumer/costumer.module.ts
+++ src/app/modules/costumer/costumer.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { MainComponent } from './pages/main/main.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
