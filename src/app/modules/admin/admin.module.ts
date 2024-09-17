53d37dddac601389c26f03dc0348a86f46fbd1af
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-09-17 14:58:58 -0300

    refactor

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminRoutingModule } from './admin-routing.module';
-import { ComponentOneComponent } from './components/component-one/component-one.component';
-import { ComponentTwoComponent } from './components/component-two/component-two.component';
-import { ComponentThreeComponent } from './components/component-three/component-three.component';
-import { ComponentFourComponent } from './components/component-four/component-four.component';
+import { ComponentOneComponent } from './components/card-top-stats/component-one.component';
+import { ComponentTwoComponent } from './components/card-medium-recepits/component-two.component';
+import { ComponentThreeComponent } from './components/card-percentage/component-three.component';
+import { ComponentFourComponent } from './components/card-graph/component-four.component';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
