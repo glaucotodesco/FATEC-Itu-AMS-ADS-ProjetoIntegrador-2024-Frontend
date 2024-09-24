1acbe4d88ba76e6ee48433d1bdcad807e4a88f64
muriisantuss <muriisantuss@gmail.com>
2024-09-24 16:35:25 -0300

    :sparkles: feat: inserting the search page and injecting the route correctly

--- src/app/modules/costumer/costumer-routing.module.ts
+++ src/app/modules/costumer/costumer-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
+  {path: 'search', component: SearchComponent},
   {path: '', component: HomeComponent},
 ];
 
--- src/app/modules/costumer/costumer.module.ts
+++ src/app/modules/costumer/costumer.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { CostumerRoutingModule } from './costumer-routing.module';
+import { SearchComponent } from './pages/search/search.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
