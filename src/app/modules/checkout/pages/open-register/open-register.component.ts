478b54e4f0df9708d0bd667ac60cd221bd01eee1
João Otávio <jofs2006@gmail.com>
2024-09-24 17:01:14 -0300

    :sparkles: feat: Added some changes

--- src/app/modules/checkout/pages/open-register/open-register.component.ts
+++ src/app/modules/checkout/pages/open-register/open-register.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './open-register.component.css'
 })
 export class OpenRegisterComponent {
+salvar() {
+    const valorInicial = (document.getElementById('valorInicial') as HTMLInputElement).value;
+    alert('Valor salvo: R$ ' + valorInicial);
 
+    const valorTotal = valorInicial ? 'R$ ' + parseFloat(valorInicial).toFixed(2) : 'R$ 0.00';
+    document.getElementById('valorTotal')!.textContent = valorTotal;
+  }
 }
