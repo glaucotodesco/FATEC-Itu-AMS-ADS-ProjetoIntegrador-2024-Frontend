02c7c6f884e7d60d4821845f0b0ba96ae1092253
glaucotodesco <glauco.todesco@gmail.com>
2024-09-24 14:57:32 -0300

    Fixed routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'costumer', pathMatch: 'full' },
   {
     path: 'costumer',
     loadChildren: () => import('./modules/costumer/costumer.module').then(m => m.CostumerModule)
