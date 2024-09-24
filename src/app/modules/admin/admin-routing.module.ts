4936dafe025dbfc6ea1ae6ee7e67faf75144b99b
vinisvp <viniciuspedro0905@gmail.com>
2024-09-24 16:23:56 -0300

    :construction:	fix: Adjusted the merge with dev

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ProductsComponent } from "./pages/products/products.component";
+import { CategoriesComponent } from "./pages/categories/categories.component";
 
 const routes: Routes = [
   {
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,6 +5,8 @@ import { CategoryTableComponent } from './components/category-table/category-tab
 import { AdminRoutingModule } from './admin-routing.module';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from '../../shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
