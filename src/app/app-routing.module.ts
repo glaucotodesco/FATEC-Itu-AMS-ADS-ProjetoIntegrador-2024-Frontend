da3a143b0171e654e75d6f824a7d310145353377
glaucotodesco <glauco.todesco@gmail.com>
2024-09-24 14:40:41 -0300

    fixed: routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-
-    path: 'waiter',
+  {  path: 'waiter',
     loadChildren: () => import('./modules/waiter/waiter.module').then(m => m.WaiterModule)
 
+  },
+  {
     path: 'admin',
     loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)
-
   }
 ];
 
