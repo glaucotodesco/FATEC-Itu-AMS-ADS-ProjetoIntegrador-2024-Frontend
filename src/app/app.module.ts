2183f4f9de116f407b898f58363dc462badac7a5
vinisvp <viniciuspedro0905@gmail.com>
2024-09-24 13:44:12 -0300

    :sparkles: feat: Adjusted the route of admin and categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AdminModule } from "./modules/admin/admin.module";
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    AdminModule,
     SharedModule
 ],
   providers: [],
