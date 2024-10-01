0a7f1a3ae466ffd35a03f3c49526868a478ac8f8
rafaScalet <rafaelscalet2018@gmail.com>
2024-10-01 15:02:08 -0300

    :sparkles: feat: add waiter menu page

--- src/app/modules/waiter/waiter-routing.module.ts
+++ src/app/modules/waiter/waiter-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { MenuItemComponent } from '../../shared/components/menu-item/menu-item.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'menu', component: MenuItemComponent },
+  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
