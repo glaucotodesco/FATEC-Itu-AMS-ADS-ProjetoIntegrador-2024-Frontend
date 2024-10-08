e6ac5ed7de307faa13cd7920fd0e8b3c85a1a18d
Lucas <lucas.desouzaca730@gmail.com>
2024-10-08 14:51:04 -0300

    :sparkles: feat: add router side-menu

--- src/app/modules/costumer/costumer-routing.module.ts
+++ src/app/modules/costumer/costumer-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'main', component: MainComponent},
 ];
 
 @NgModule({
---
+++ src/app/modules/costumer/pages/main/components/side-menu/side-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+
+}
