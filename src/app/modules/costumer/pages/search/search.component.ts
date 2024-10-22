72d189cc4c8d470f706a68840a0147f22af9c473
muriisantuss <muriisantuss@gmail.com>
2024-10-22 17:13:55 -0300

    :sparkles: feat: implementando o selecionar o item e ficar estatico

--- src/app/modules/costumer/pages/search/search.component.ts
+++ src/app/modules/costumer/pages/search/search.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  items = [
+    { label: 'Entradas', imageUrl: 'assets/costumer/search/entradas.jpg' },
+    { label: 'Prato Principal', imageUrl: 'assets/costumer/search/prato_principal.jpg' },
+    { label: 'Bebidas', imageUrl: 'assets/costumer/search/bebidas.jpg' },
+    { label: 'Porções', imageUrl: 'assets/costumer/search/porcoes.jpg' },
+    { label: 'Sobremesa', imageUrl: 'assets/costumer/search/sobremesa.jpg' }
+  ];
 
+  selectedCard: any = null;
+
+  selectCard(card: any) {
+    this.selectedCard = this.selectedCard === card ? null : card; // Alterna a seleção
+  }
 }
